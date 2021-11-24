e69118836a970203864e0c51648cbd611c75225b
Alice Medvedeva <adelaide3alice@gmail.com>
2021-11-25 00:37:35 +0500

    all

--- src/features/ProductList/ProductList.tsx
+++ src/features/ProductList/ProductList.tsx
@@ -1,6 +1,11 @@
 import { useRouter } from 'next/router';
 import { Table, Popconfirm } from 'antd';
 import { TablePaginationConfig } from 'antd/lib/table';
+import { useProductListQuery } from './__generated__/ProductListQuery';
+import { useProductDeleteMutation } from 'app/features/ProductList/__generated__/ProductDeleteMutation';
+import { useProductRemoveByIdMutation } from 'app/features/ProductList/__generated__/ProductRemoveByIdMutation';
+import { useProductSetPriceMutation } from 'app/features/ProductList/__generated__/ProductSetPriceMutation';
+import { useProductUpdateSubscription } from 'app/features/ProductList/__generated__/ProductUpdateSubscription';
 
 export function ProductList() {
   const router = useRouter();
@@ -8,13 +13,41 @@ export function ProductList() {
     page: parseInt(router.query?.page as any) || 1,
     perPage: parseInt(router.query?.perPage as any) || 5,
   };
-  const loading = false;
+
+  // и просто вызываем наш хук!
+  const { data, loading, error } = useProductListQuery({
+    variables,
+  });
+
+  // мутация - удаление элемента
+  const [removeById] = useProductRemoveByIdMutation({
+    update( cashe, result) {
+      // Apollo cashe бывает кастомным. И по умолчанию аполо ставит в id поле id -
+      // но вы можете это переопределить с помощью identify и evict
+      //суть в том что без этих двух строк приходилось каждый раз обновлять таблицу
+      // чтобы посмотреть результат удаления - а тут сразу автоматически запись убирается
+      const id = cashe.identify(result.data?.removeProduct.record);
+      cashe.evict({id});
+    }
+  });
+
+  // мутация изменения цены
+  const [setPrice] = useProductSetPriceMutation();
+
+  // подписка на обновление записи - а именно на обновление ее цены
+  // зачем это? Чтобы если ты изменяешь цену в одном окне браузера -
+  // в другом у тебя отобразилось изменение цены автоматически
+
+  // но не стоит вводить много подписок
+  // 1) это жуко тормозит - слишком огромная нагрузка на сервер
+  // 2)бекам великая запара
+  useProductUpdateSubscription();
 
   return (
     <Table
-      title={() => <h1>TODO: Implement ProductList logic like in Orders</h1>}
+      title={() => <h1>Implement ProductList logic like in Orders</h1>}
       loading={loading}
-      dataSource={[]}
+      dataSource={data?.viewer?.productPagination?.items || []}
       columns={[
         {
           title: 'ProductID',
@@ -32,7 +65,15 @@ export function ProductList() {
           title: 'Unit Price',
           dataIndex: 'unitPrice',
           render: (unitPrice, _record) => {
-            return unitPrice;
+            return (<span onClick={() => {
+              setPrice({
+             variables: {
+                _id: _record._id,
+                  newUnitPrice: _record.unitPrice * 1.1
+              }
+            });
+            }
+            }>unitPrice</span>);
             // return <OrderListEditableFreight record={record} />;
           },
         },
@@ -44,7 +85,9 @@ export function ProductList() {
               <Popconfirm
                 title="Sure to delete?"
                 onConfirm={() => {
-                  alert('run delete mutation');
+                  removeById({
+                    variables: { _id: _record?._id },
+                  });
                 }}
               >
                 <a>Delete</a>
@@ -54,12 +97,19 @@ export function ProductList() {
         },
       ]}
       pagination={{
+        // showSizeChanger: true,
+        // pageSize: variables.perPage,
+        // current: variables.page,
+        // pageSizeOptions: ['5', '10', '100'],
+        // total: 100, // TODO: from graphql response
+        // А нафига клиенту считать какую страницу прислал бек - возьмем данные с бека!
         showSizeChanger: true,
-        pageSize: variables.perPage,
-        current: variables.page,
+        pageSize: data?.viewer?.productPagination?.pageInfo?.perPage,
+        current: data?.viewer?.productPagination?.pageInfo?.currentPage,
         pageSizeOptions: ['5', '10', '100'],
-        total: 100, // TODO: from graphql response
+        total: data?.viewer?.productPagination?.count,
       }}
+      // дада все изменения через адресную строку
       onChange={(pagination: TablePaginationConfig) => {
         router.push({
           pathname: router.pathname,
